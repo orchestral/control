031ec13f9d8986819b1c50b4bc31bae7ccedbf75
crynobone <crynobone@gmail.com>
2013-06-04 21:07:50 +0800

    Add getSync method.

--- src/controllers/AclController.php
+++ src/controllers/AclController.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\View;
 use Orchestra\Support\Facades\App;
 use Orchestra\Support\Facades\Acl;
+use Orchestra\Support\Facades\Messages;
 use Orchestra\Support\Facades\Site;
 use Orchestra\Support\Str;
 
@@ -54,5 +55,32 @@ class AclController extends BaseController {
 		Site::set('title', trans('orchestra/control::title.acls.list'));
 
 		return View::make('orchestra/control::acl.index', $data);
+	}
+
+	/**
+	 * Get sync roles action.
+	 *
+	 * @access public
+	 * @param  string   $name
+	 * @return Response
+	 */
+	public function getSync($name)
+	{
+		$roles = array();
+		$acls  = Acl::all();
+
+		if ( ! isset($acls[$name])) return App::abort(404);
+
+		$current = $acls[$name];
+
+		foreach (Role::all() as $role) $roles[] = $role->name;
+
+		$current->roles()->add($roles);
+		
+		Messages::add('success', trans('orchestra/control::response.acls.sync-roles', array(
+			'name' => Str::humanize($name),
+		)));
+
+		return Redirect::to(handles("orchestra/foundation::resources/control.acls?name={$name}"));
 	}
 }
