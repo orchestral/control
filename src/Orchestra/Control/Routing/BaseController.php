b8d943795517009715bd31066bf80ecde71c9868
crynobone <crynobone@gmail.com>
2013-10-03 17:26:47 +0800

    Update to Laravel 4.1 routing
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Orchestra/Control/Routing/BaseController.php
+++ src/Orchestra/Control/Routing/BaseController.php
@@ -1,5 +1,5 @@
 <?php namespace Orchestra\Control\Routing;
 
-use Illuminate\Routing\Controllers\Controller;
+use Illuminate\Routing\Controller;
 
 abstract class BaseController extends Controller {}
