39525141916bf92837a05cf9f845fd02a2c7b128
crynobone <crynobone@gmail.com>
2013-10-11 21:19:08 +0800

    Move routing to event, would make it only called when hitting the admin route.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/routes.php
+++ src/routes.php
@@ -3,16 +3,29 @@
 use Orchestra\Support\Facades\App;
 use Orchestra\Support\Facades\Resources;
 
-$control = Resources::make('control', array(
-	'name'    => 'Control',
-	'uses'    => 'restful:Orchestra\Control\Routing\HomeController',
-	'visible' => function ()
-	{
-		$acl = App::acl();
-		return ($acl->can('manage acl') or $acl->can('manage roles'));
-	}
-));
+/*
+|--------------------------------------------------------------------------
+| Control Resources Route
+|--------------------------------------------------------------------------
+|
+| Register Control Extension as a resources.
+|
+*/
 
-$control['roles']  = 'resource:Orchestra\Control\Routing\RolesController';
-$control['acl']    = 'restful:Orchestra\Control\Routing\AclController';
-$control['themes'] = 'restful:Orchestra\Control\Routing\ThemesController';
+Event::listen('orchestra.started: admin', function ()
+{
+	$control = Resources::make('control', array(
+		'name'    => 'Control',
+		'uses'    => 'restful:Orchestra\Control\Routing\HomeController',
+		'visible' => function ()
+		{
+			$acl = App::acl();
+			return ($acl->can('manage acl') or $acl->can('manage roles'));
+		}
+	));
+
+	$control['roles']  = 'resource:Orchestra\Control\Routing\RolesController';
+	$control['acl']    = 'restful:Orchestra\Control\Routing\AclController';
+	$control['themes'] = 'restful:Orchestra\Control\Routing\ThemesController';
+
+});
