81db1b20b7df301bc90c1a375644af5453fa541f
crynobone <crynobone@gmail.com>
2013-12-14 22:42:21 +0800

    remove some unrelevant docblock.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Orchestra/Control/Validation/Role.php
+++ src/Orchestra/Control/Validation/Role.php
@@ -16,7 +16,6 @@ class Role extends Validator
     /**
      * On create validations.
      *
-     * @access public
      * @return void
      */
     protected function onCreate()
@@ -27,7 +26,6 @@ class Role extends Validator
     /**
      * On update validations.
      *
-     * @access public
      * @return void
      */
     protected function onUpdate()
