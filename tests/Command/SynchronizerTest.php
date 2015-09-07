d5bfe633cc31704dad1492c5ef6f4bf355647ae4
crynobone <crynobone@gmail.com>
2015-09-07 08:13:30 +0800

    Convert to phpunit.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ tests/Command/SynchronizerTest.php
@@ -0,0 +1,48 @@
+<?php namespace Orchestra\Control\TestCase;
+
+use Mockery as m;
+use Orchestra\Model\Role;
+use Orchestra\Control\Command\Synchronizer;
+use Illuminate\Contracts\Foundation\Application;
+use Orchestra\Contracts\Authorization\Authorization;
+use Orchestra\Control\Contracts\Command\Synchronizer as SynchronizerContract;
+
+class SynchronizerTest extends \PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testItIsInitializable()
+    {
+        $app = m::mock(Application::class);
+        $acl = m::mock(Authorization::class);
+
+        $stub = new Synchronizer($app, $acl);
+
+        $this->assertInstanceOf(Synchronizer::class, $stub);
+        $this->assertInstanceOf(SynchronizerContract::class, $stub);
+    }
+
+    public function testItCanBeSynced()
+    {
+
+        $app  = m::mock(Application::class);
+        $acl  = m::mock(Authorization::class);
+        $role = m::mock(Role::class);
+
+        $stub = new Synchronizer($app, $acl);
+
+        $admin = (object) ['id' => 1, 'name' => 'Administrator'];
+
+        $app->shouldReceive('make')->once()->with('orchestra.role')->andReturn($role);
+        $role->shouldReceive('admin')->once()->andReturn($admin);
+
+        $acl->shouldReceive('allow')->once()
+            ->with('Administrator', ['Manage Users', 'Manage Orchestra', 'Manage Roles', 'Manage Acl'])
+            ->andReturnNull();
+
+        $this->assertNull($stub->handle());
+    }
+}
---
+++ tests/Http/Handlers/ControlMenuHandlerTest.php
@@ -0,0 +1,54 @@
+<?php namespace Orchestra\Control\Http\Handlers\TestCase;
+
+use Mockery as m;
+use Orchestra\Widget\Handlers\Menu;
+use Orchestra\Foundation\Support\MenuHandler;
+use Illuminate\Contracts\Foundation\Application;
+use Orchestra\Control\Http\Handlers\ControlMenuHandler;
+
+class ControlMenuHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testItIsInitializable()
+    {
+        $app  = m::mock(Application::class);
+        $menu = m::mock(Menu::class);
+
+        $app->shouldReceive('make')->once()->with('orchestra.platform.menu')->andReturn($menu);
+
+        $stub = new ControlMenuHandler($app);
+
+        $this->assertInstanceOf(ControlMenuHandler::class, $stub);
+        $this->assertInstanceOf(MenuHandler::class, $stub);
+    }
+
+    public function testItShouldBeChildOfExtensionGivenExtensionIsAvailable()
+    {
+        $app  = m::mock(Application::class);
+        $menu = m::mock(Menu::class);
+
+        $app->shouldReceive('make')->once()->with('orchestra.platform.menu')->andReturn($menu);
+        $menu->shouldReceive('has')->once()->with('extensions')->andReturn(true);
+
+        $stub = new ControlMenuHandler($app);
+
+        $this->assertEquals('^:extensions', $stub->getPositionAttribute());
+    }
+
+    public function testItShouldNextToHomeGivenExtensionIsntAvailable()
+    {
+        $app  = m::mock(Application::class);
+        $menu = m::mock(Menu::class);
+
+        $app->shouldReceive('make')->once()->with('orchestra.platform.menu')->andReturn($menu);
+        $menu->shouldReceive('has')->once()->with('extensions')->andReturn(false);
+
+        $stub = new ControlMenuHandler($app);
+
+        $this->assertEquals('>:home', $stub->getPositionAttribute());
+    }
+}
