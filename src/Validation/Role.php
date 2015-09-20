ef130f0fb2fb0f218984ea55e5880bd9aa3e522e
crynobone <crynobone@gmail.com>
2015-09-20 17:10:54 +0800

    Disallow reserve word "guest".
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Validation/Role.php
+++ src/Validation/Role.php
@@ -10,7 +10,7 @@ class Role extends Validator
      * @var array
      */
     protected $rules = [
-        'name' => ['required'],
+        'name' => ['required', 'not_in:guest'],
     ];
 
     /**
