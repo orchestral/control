7fca954f0beb6d4ba03781eb4466165ff96cd43b
crynobone <crynobone@gmail.com>
2015-09-20 20:15:42 +0800

    Disallow reserved keyword to be use for roles.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- resources/lang/en/response.php
+++ resources/lang/en/response.php
@@ -2,16 +2,17 @@
 
 return [
     'acls' => [
-        'update'     => 'ACL has been updated',
-        'sync-roles' => 'Role has been synced to :name metric',
+        'update'     => 'ACL has been updated.',
+        'sync-roles' => 'Role has been synced to :name metric.',
     ],
 
     'roles' => [
-        'create' => 'Role :name has been created',
-        'update' => 'Role :name has been updated',
-        'delete' => 'Role :name has been deleted',
+        'create' => 'Role :name has been created.',
+        'update' => 'Role :name has been updated.',
+        'delete' => 'Role :name has been deleted.',
 
-        'delete-default-failed' => 'Unable to delete default role',
+        'delete-default-failed' => 'Unable to delete default role.',
+        'reserved-word'         => 'Unable to use following word as role.',
     ],
 
     'themes' => [
--- src/Validation/Role.php
+++ src/Validation/Role.php
@@ -1,6 +1,9 @@
 <?php namespace Orchestra\Control\Validation;
 
+use Orchestra\Support\Keyword;
 use Orchestra\Support\Validator;
+use Orchestra\Support\Facades\Foundation;
+use Illuminate\Contracts\Validation\Validator as ValidatorResolver;
 
 class Role extends Validator
 {
@@ -40,5 +43,60 @@ class Role extends Validator
     protected function onUpdate()
     {
         $this->rules['name'][] = 'unique:roles,name,{roleID}';
+    }
+
+    /**
+     * Extend create validations.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     *
+     * @return void
+     */
+    protected function extendCreate(ValidatorResolver $validator)
+    {
+        $this->disallowReservedRoleName($validator);
+    }
+
+    /**
+     * Extend update validations.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     *
+     * @return void
+     */
+    protected function extendUpdate(ValidatorResolver $validator)
+    {
+        $this->disallowReservedRoleName($validator);
+    }
+
+    /**
+     * Disallow reseerved role name.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     *
+     * @return void
+     */
+    protected function disallowReservedRoleName(ValidatorResolver $validator)
+    {
+        $validator->after(function (ValidatorResolver $v) {
+            if ($this->roleNameIsAlreadyUsed($v->getData()['name'])) {
+                $v->errors()->add('name', trans('orchestra/control::response.roles.reserved-word'));
+            }
+        });
+    }
+
+    /**
+     * Check if role name is already used.
+     *
+     * @param  string  $name
+     *
+     * @return bool
+     */
+    protected function roleNameIsAlreadyUsed($name)
+    {
+        $keyword = Keyword::make($name);
+        $roles   = Foundation::acl()->roles()->get();
+
+        return $keyword->searchIn($roles) >= 0;
     }
 }
